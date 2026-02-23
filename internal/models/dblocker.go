5de0cbd5cc74b749fa8b0a020942261242e7bf9e
CahyaAji <cahya.akk@gmail.com>
2026-02-23 09:46:09 +0700

    add dblocker model

---
+++ internal/models/dblocker.go
@@ -0,0 +1,22 @@
+package models
+
+type DBlockerConfig struct {
+	SignalGPS  bool `json:"signal_gps" default:"false"`
+	SignalCtrl bool `json:"signal_ctrl" default:"false"`
+}
+
+type DBlocker struct {
+	ID         uint             `gorm:"primaryKey" json:"id"`
+	Name       string           `json:"name" binding:"required"`
+	SerialNumb string           `json:"serial_numb" binding:"required"`
+	Lat        float64          `json:"latitude" binding:"required"`
+	Lng        float64          `json:"longitude" binding:"required"`
+	Desc       string           `json:"desc"`
+	AngleStart int              `json:"angle_start" default:"0"`
+	Config     []DBlockerConfig `gorm:"serializer:json;type:jsonb" json:"config"`
+}
+
+// type DBlockerConfigUpdate struct {
+// 	ID     uint             `json:"id" binding:"required"`
+// 	Config []DBlockerConfig `gorm:"serializer:json;type:jsonb" json:"config" required:"true"`
+// }
