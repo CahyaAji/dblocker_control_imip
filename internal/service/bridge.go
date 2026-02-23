75549f4a976b81f7c3196dc84e360451d8f9b04e
CahyaAji <cahya.akk@gmail.com>
2026-02-23 14:50:48 +0700

    change topic call method

--- internal/service/bridge.go
+++ internal/service/bridge.go
@@ -2,12 +2,15 @@ package service
 
 import (
 	"dblocker_control/internal/infrastructure/mqtt"
+	"dblocker_control/internal/models"
+	"fmt"
+	"strings"
 	"sync"
 )
 
 // BridgeService subscribes to MQTT and fans messages to subscribers.
 type BridgeService struct {
-	topic       string
+	topics      []string
 	broadcaster *broadcaster
 }
 
@@ -20,19 +23,51 @@ func newBroadcaster() *broadcaster {
 	return &broadcaster{subscribers: make(map[chan mqtt.Message]struct{})}
 }
 
+type DBlockerReader interface {
+	FindAll() ([]models.DBlocker, error)
+}
+
 // NewBridgeService wires the MQTT subscription to the broadcaster.
-func NewBridgeService(client mqtt.Client, topic string) (*BridgeService, error) {
-	br := &BridgeService{topic: topic, broadcaster: newBroadcaster()}
-	if err := client.Subscribe(topic, 0, func(msg mqtt.Message) {
-		br.broadcast(msg)
-	}); err != nil {
+
+func NewBridgeService(client mqtt.Client, dblockerReader DBlockerReader) (*BridgeService, error) {
+	dblockers, err := dblockerReader.FindAll()
+	if err != nil {
 		return nil, err
 	}
+
+	br := &BridgeService{topics: make([]string, 0, len(dblockers)), broadcaster: newBroadcaster()}
+	seenTopics := make(map[string]struct{})
+
+	for _, dblocker := range dblockers {
+		serial := strings.TrimSpace(dblocker.SerialNumb)
+		if serial == "" {
+			continue
+		}
+
+		topic := fmt.Sprintf("dbl/%s/rpt", serial)
+		if _, exists := seenTopics[topic]; exists {
+			continue
+		}
+
+		if err := client.Subscribe(topic, 0, func(msg mqtt.Message) {
+			br.broadcast(msg)
+		}); err != nil {
+			return nil, err
+		}
+
+		seenTopics[topic] = struct{}{}
+		br.topics = append(br.topics, topic)
+	}
+
+	if len(br.topics) == 0 {
+		return nil, fmt.Errorf("no dblocker serial numbers found to subscribe")
+	}
+
 	return br, nil
 }
 
 func (b *BridgeService) Topic() string {
-	return b.topic
+	return strings.Join(b.topics, ",")
 }
 
 func (b *BridgeService) Subscribe() chan mqtt.Message {
