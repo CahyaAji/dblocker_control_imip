095ebc9da4c9a4eb23f7086ac384b208178ffd5e
CahyaAji <cahya.akk@gmail.com>
2026-02-24 09:37:36 +0700

    fix topic subcribtion bugs

--- internal/service/bridge.go
+++ internal/service/bridge.go
@@ -3,8 +3,6 @@ package service
 import (
 	"dblocker_control/internal/infrastructure/mqtt"
 	"dblocker_control/internal/models"
-	"fmt"
-	"strings"
 	"sync"
 )
 
@@ -29,45 +27,26 @@ type DBlockerReader interface {
 
 // NewBridgeService wires the MQTT subscription to the broadcaster.
 
-func NewBridgeService(client mqtt.Client, dblockerReader DBlockerReader) (*BridgeService, error) {
-	dblockers, err := dblockerReader.FindAll()
-	if err != nil {
+func NewBridgeService(client mqtt.Client, _ DBlockerReader) (*BridgeService, error) {
+	br := &BridgeService{topics: make([]string, 0, 1), broadcaster: newBroadcaster()}
+	topic := "dbl/+/rpt"
+
+	if err := client.Subscribe(topic, 0, func(msg mqtt.Message) {
+		br.broadcast(msg)
+	}); err != nil {
 		return nil, err
 	}
 
-	br := &BridgeService{topics: make([]string, 0, len(dblockers)), broadcaster: newBroadcaster()}
-	seenTopics := make(map[string]struct{})
-
-	for _, dblocker := range dblockers {
-		serial := strings.TrimSpace(dblocker.SerialNumb)
-		if serial == "" {
-			continue
-		}
-
-		topic := fmt.Sprintf("dbl/%s/rpt", serial)
-		if _, exists := seenTopics[topic]; exists {
-			continue
-		}
-
-		if err := client.Subscribe(topic, 0, func(msg mqtt.Message) {
-			br.broadcast(msg)
-		}); err != nil {
-			return nil, err
-		}
-
-		seenTopics[topic] = struct{}{}
-		br.topics = append(br.topics, topic)
-	}
-
-	if len(br.topics) == 0 {
-		return nil, fmt.Errorf("no dblocker serial numbers found to subscribe")
-	}
-
+	br.topics = append(br.topics, topic)
 	return br, nil
 }
 
 func (b *BridgeService) Topic() string {
-	return strings.Join(b.topics, ",")
+	if len(b.topics) == 0 {
+		return ""
+	}
+
+	return b.topics[0]
 }
 
 func (b *BridgeService) Subscribe() chan mqtt.Message {
